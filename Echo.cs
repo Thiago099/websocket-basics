132eea1256f3b7d83fd0be9a4e056300b9307456
Thiago099 <tecnologiasthiago@gmail.com>
2022-06-26 12:49:03 -0300

    first commit

---
+++ Echo.cs
@@ -0,0 +1,12 @@
+﻿using WebSocketSharp;
+using WebSocketSharp.Server;
+internal class Echo : WebSocketBehavior
+{
+    protected override void OnMessage(MessageEventArgs e)
+    {
+        Console.WriteLine("Recived message from client: " + e.Data);
+        Send(e.Data);
+        Sessions.Broadcast(e.Data);
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using WebSocketSharp;
+using WebSocketSharp.Server;
+
+string adress = "localhost:7890";
+WebSocketServer server = new WebSocketServer($"ws://{adress}");
+server.AddWebSocketService<Echo>("/Echo");
+
+server.Start();
+Console.WriteLine($"Server started on {adress}");
+Console.ReadKey();
+server.Stop();
